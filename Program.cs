070a50e45260143488e7067debbc668afd581cc9
m988ar <vborja2003@gmail.com>
2026-05-23 16:13:55 -0300

    Punto 1 terminado

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿Console.WriteLine("Hello, World!");
 int a;
-
 int b;
+a=10;
+b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);
 
-a = 10;
+Console.WriteLine("Ingrese un numero");
+string numero = Console.ReadLine();
 
-b = a;
-Console.WriteLine("Valor de a:"+a);
-Console.WriteLine("Valor de b:"+b);
+if (int.TryParse(numero, out int numeroInvertir))
+{
+    if (numeroInvertir > 0)
+    {
+        int nuevoNumero = 0;
+        while (numeroInvertir > 0)
+        {
+            int aux = numeroInvertir % 10;
+            nuevoNumero = nuevoNumero * 10 + aux;
+            numeroInvertir /= 10;
+        }
+        Console.WriteLine("Numero invertido: " +  nuevoNumero);
+    }
+    else
+    {
+        Console.WriteLine("No es un numero mayor a 0");
+    }
+}
+else
+{
+    Console.WriteLine("Ingrese un numero valido");
+}
