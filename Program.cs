7a01be232c28510415b6f5d16afe551c4f8d6bb5
m988ar <vborja2003@gmail.com>
2026-05-23 18:03:32 -0300

    Calculadora V2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*Console.WriteLine("Hello, World!");
 int a;
 int b;
 a=10;
@@ -30,4 +30,66 @@ if (int.TryParse(numero, out int numeroInvertir))
 else
 {
     Console.WriteLine("Ingrese un numero valido");
-}
+}*/
+int indice = 0;
+string menu;
+
+do
+{
+    Console.WriteLine("Elija una opcion");
+    Console.WriteLine("1)Valor Absoluto");
+    Console.WriteLine("2)Cuadrado");
+    Console.WriteLine("3)Raiz cuadrada");
+    Console.WriteLine("4)Seno");
+    Console.WriteLine("5)Coseno");
+    Console.WriteLine("6)Parte entera");
+    menu = Console.ReadLine();
+    if (int.TryParse(menu, out indice) && indice < 7 && indice > 0)
+    {
+        Console.WriteLine("Ingrese el numero");
+
+        if (!float.TryParse(Console.ReadLine(), out float num1))
+        {
+            System.Console.WriteLine("Ingrese un valor valido");
+        }
+        else
+        {
+            float resultado = 0;
+            switch (indice)
+            {
+                case 1:
+                    if (num1 > 0)
+                    {
+                        resultado = num1;
+                    }
+                    else
+                    {
+                        resultado = -1 * num1;
+                    }
+                    break;
+                case 2:
+                    resultado = num1 * num1;
+                    break;
+                case 3:
+                    resultado = (float)Math.Sqrt(num1);
+                    break;
+                case 4:
+                    resultado = (int)Math.Sin(num1 * Math.PI / 180);
+                    break;
+                case 5:
+                    resultado = (int)Math.Cos(num1 * Math.PI / 180);
+                    break;
+                case 6:
+                    resultado = (int)num1;
+                    break;
+            }
+            Console.WriteLine("Resultado: " + resultado);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Elija una opcion valida");
+    }
+    Console.WriteLine("Desea realizar otro calculo? \n1)Si\n0)No");
+    menu = Console.ReadLine();
+} while (int.TryParse(menu, out indice) && indice < 5 && indice > 0);
