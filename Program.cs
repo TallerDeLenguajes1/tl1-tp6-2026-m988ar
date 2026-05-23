beb98bacb58a61898355d6c6c470e49ecde884c5
m988ar <vborja2003@gmail.com>
2026-05-23 17:30:59 -0300

    Ejercicio 3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿Console.WriteLine("Hello, World!");
+/*
+//Ejercicio 1
 int a;
 int b;
 a=10;
@@ -30,4 +32,66 @@ if (int.TryParse(numero, out int numeroInvertir))
 else
 {
     Console.WriteLine("Ingrese un numero valido");
-}
+}*/
+
+//Ejercicio 2
+
+int indice = 0;
+string menu;
+
+do
+{
+    Console.WriteLine("Elija una opcion");
+    Console.WriteLine("1)Sumar");
+    Console.WriteLine("2)Restar");
+    Console.WriteLine("3)Multiplicar");
+    Console.WriteLine("4)Dividir");
+    menu = Console.ReadLine();
+    if (int.TryParse(menu, out indice) && indice < 5 && indice > 0)
+    {
+        Console.WriteLine("Ingrese el 1er numero");
+
+        if (!int.TryParse(Console.ReadLine(), out int num1))
+        {
+            continue;
+        }
+
+        Console.WriteLine("Ingrese el 2do numero");
+
+        if (!int.TryParse(Console.ReadLine(), out int num2))
+        {
+            continue;
+        }
+
+        int resultado = 0;
+        switch (indice)
+        {
+            case 1:
+                resultado = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            case 3:
+                resultado = num1 * num2;
+                break;
+            case 4:
+                if (num2 != 0)
+                {
+                    resultado = num1 / num2;    
+                }
+                else
+                {
+                    Console.WriteLine("SyntaxError");
+                }
+                break;
+        }
+        Console.WriteLine("Resultado: " + resultado);
+    }
+    else
+    {
+        Console.WriteLine("Elija una opcion valida");
+    }
+    Console.WriteLine("Desea realizar otro calculo? \n1)Si\n0)No");
+    menu = Console.ReadLine();
+} while (int.TryParse(menu, out indice) && indice < 5 && indice > 0);
