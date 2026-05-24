b427c733dc3be92b72dd12e1f85cbac5178df863
m988ar <vborja2003@gmail.com>
2026-05-24 14:28:40 -0300

    Merge de calculadora

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*Console.WriteLine("Hello, World!");
 int a;
 int b;
 a=10;
@@ -30,4 +30,25 @@ if (int.TryParse(numero, out int numeroInvertir))
 else
 {
     Console.WriteLine("Ingrese un numero valido");
-}
+} */
+
+//Ejercicio 4
+
+System.Console.WriteLine("Ingrese una cadena de texto");
+string cad1 = Console.ReadLine();
+System.Console.WriteLine("La longitud de la cadena es de " + cad1.Length + " caracteres");
+
+System.Console.WriteLine("Ingrese ota cadena de texto");
+string cad2 = Console.ReadLine();
+string cadConcat = $"{cad1}{cad2}";
+System.Console.WriteLine(cadConcat);
+
+Console.Write("Desde qué posición quiere extraer: ");
+int inicio = int.Parse(Console.ReadLine());
+
+Console.Write("Cuántos caracteres quiere extraer: ");
+int cantidad = int.Parse(Console.ReadLine());
+
+string subcadena = cad1.Substring(inicio, cantidad);
+
+Console.WriteLine("Subcadena extraída: " + subcadena);
---
+++ README.md
@@ -0,0 +1,8 @@
+# TP 6 Borja Luca Valentin
+
+## ¿String es una tipo por valor o un tipo por referencia?
+
+
+## ¿Qué secuencias de escape tiene el tipo string?
+
+## ¿Qué sucede cuando utiliza el carácter @ y $ antes de una cadena de texto?
